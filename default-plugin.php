565eb2d7f71a85876a0a03f9238491a21df15e10
Fred Bradley <hello@fredbradley.co.uk>
2019-06-20 04:27:39 +0000

    Initial commit

---
+++ default-plugin.php
@@ -0,0 +1,10 @@
+<?php
+	/*
+        Plugin Name: Cranleigh Default Plugin
+        Description: Description
+        Author: Fred Bradley
+        Version: 1.0
+        Author URI: http://fred.im/
+    */
+
+	require_once 'vendor/autoload.php';
